c5f05e67bda9ffdb881cad7b9acb9644dda60339
chen-sella <chensella1@gmail.com>
2021-05-10 19:01:54 +0300

    uploading to heroku

--- server.js
+++ server.js
@@ -6,18 +6,10 @@ app.use(express.static(__dirname + "/dist/crm-dev"));
 
 app.get("/**", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "crm-dev", "index.html"));
-  //   res.sendFile("index.html", { root: "dist/crm-dev/" });
 });
 
 app.listen(process.env.PORT || 8080);
 console.log("Console listening!");
 console.log(__dirname, 'dirname');
 
-// function requireHTTPS(req, res, next) {
-//   if (!req.secure && req.get("x-forwarded-proto") !== "https") {
-//     return res.redirect("https://" + req.get("host") + req.url);
-//   }
-//   next();
-// }
-// app.use(express.static("./dist/crm-dev"));
-// app.use(requireHTTPS);
+
