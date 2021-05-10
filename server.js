eda3c17a5cb54dfd8339195adfeaee60c0de624f
chen-sella <chensella1@gmail.com>
2021-05-10 18:29:30 +0300

    server edits

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require("path");
 app.use(express.static(__dirname + "/dist/crm-dev"));
 
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/dist/crm-dev/index.html"));
-    //   res.sendFile("index.html", { root: "dist/crm-dev/" });
+  res.sendFile(path.join(__dirname, "dist", "crm-dev", "index.html"));
+  //   res.sendFile("index.html", { root: "dist/crm-dev/" });
 });
 
 app.listen(process.env.PORT || 8080);
-console.log('Console listening!');
+console.log("Console listening!");
 
 // function requireHTTPS(req, res, next) {
 //   if (!req.secure && req.get("x-forwarded-proto") !== "https") {
